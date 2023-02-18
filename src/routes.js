3e115267c8c6079cd0e91f4755f44c42981d93f3
pedronaalberto <76451233+pedronaalberto@users.noreply.github.com>
2023-02-18 17:04:47 +0000

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,36 @@ import { renderRoutes } from "react-router-config";
 import { Switch } from "react-router-dom";
 import routesConfig from "./routesConfig";
 
-const Routes = () => {
-  return <Switch>{renderRoutes(routesConfig)}</Switch>;
-};
+const routes = [
+  {
+    path: '/api/burns/all',
+    exact: true,
+    component: BurnsController.getAllBurns
+  },
+  {
+    path: '/api/burns/:id',
+    exact: true,
+    component: BurnsController.getBurn
+  },
+  {
+    path: '/api/burns/create',
+    exact: true,
+    component: BurnsController.createBurn
+  },
+  {
+    path: '/api/burns/update/:id',
+    exact: true,
+    component: BurnsController.updateBurn
+  },
+  {
+    path: '/api/burns/delete/:id',
+    exact: true,
+    component: BurnsController.deleteBurn
+  },
+  {
+    path: '/',
+    component: Layout
+  }
+];
 
-export default Routes;
+export default routes;
